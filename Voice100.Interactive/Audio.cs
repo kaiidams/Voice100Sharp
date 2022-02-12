c45e7b08dc6d11db0b75aa89aa4c7bc558a00979
Katsuya Iida <katsuya.iida@gmail.com>
2022-02-13 08:16:35 +0900

    Added DotNet Interactive support

---
+++ Voice100.Interactive/Audio.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Voice100.Interactive
+{
+    public class Audio
+    {
+        public const string MimeType = "audio/wav";
+
+        private static string GetDataURL(string mimeType, byte[] bytes)
+        {
+            return "data:" + mimeType + ";base64," + Convert.ToBase64String(bytes);
+        }
+
+        private short[] _waveform;
+        private int _rate;
+
+        public Audio(short[] waveform, int rate)
+        {
+            _waveform = waveform;
+            _rate = rate;
+        }
+
+        public string GetDataUrl()
+        {
+            byte[] bytes = WaveFile.GetBytes(_waveform, _rate, true);
+            return GetDataURL(MimeType, bytes);
+        }
+    }
+}
---
+++ Voice100.Interactive/Formatting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Voice100.Interactive
+{
+    public static class Formatting
+    {
+        public static Audio Audio(short[] waveform, int rate)
+        {
+            return new Audio(waveform, rate);
+        }
+    }
+}
